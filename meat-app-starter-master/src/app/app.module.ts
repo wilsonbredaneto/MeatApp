7e33fd244077c11a8c1bbac5d6f6865b3ad2d291
wilsonbredaneto <wilsonbredaneto@gmail.com>
2019-01-08 17:13:56 -0200

    Criacao de Formulario

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { ROUTES } from './app.routes'
+import { ROUTES } from './app.routes';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,6 +19,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shoppint-cart.service';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
@@ -31,11 +33,13 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shoppint-
     MenuComponent,
     ShoppingCartComponent,
     MenuItemComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
