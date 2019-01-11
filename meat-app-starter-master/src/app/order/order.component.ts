9401e565c303244ce6bf19865b134ee38f6fc48b
wilsonbredaneto <wilsonbredaneto@gmail.com>
2019-01-11 11:27:29 -0200

    Reactive Forms Parte 2

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, EmailValidator, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, EmailValidator, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -27,7 +27,6 @@ export class OrderComponent implements OnInit {
     {label: "Cartão Refeição", value: "REF"}
   ];
 
-
   constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -39,7 +38,22 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control("", [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(""),
       paymentOption: this.formBuilder.control("", [Validators.required])
-    });
+    }, {validator: OrderComponent.equalsTo});
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean}
+  {
+    const email = group.get("email");
+    const emailConfirmation = group.get("emailConfirmation");
+
+    if(!email || !emailConfirmation)
+      return undefined;
+    if(email.value !== emailConfirmation.value)
+    {
+      return {emailsNotMatch: true};
+    }
+
+    return undefined;
   }
 
   itemsValue() : number{
