79b0a1bb836f8e14190963fa87a18f0c1448ad45
wilsonbredaneto <wilsonbredaneto@gmail.com>
2019-01-14 14:01:31 -0200

    Servicos Compartilhados

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -37,8 +36,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule, 
+    SharedModule.forRoot(), 
     RouterModule.forRoot(ROUTES)
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
--- meat-app-starter-master/src/app/shared/shared.module.ts
+++ meat-app-starter-master/src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shoppint-cart.service";
+import { RestaurantService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
 
 
 
@@ -13,4 +16,11 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule]
 })
 
-export class SharedModule{}
+export class SharedModule{
+    static forRoot(): ModuleWithProviders{
+        return{
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantService, OrderService]
+        };
+    }
+}
