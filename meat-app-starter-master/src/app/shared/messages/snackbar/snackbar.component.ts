7e00023825a4c819a872c162d2a53904fe8a1a6a
wilsonbredaneto <wilsonbredaneto@gmail.com>
2019-01-25 10:58:12 -0200

    Do e SwitchMap

--- meat-app-starter-master/src/app/shared/messages/snackbar/snackbar.component.ts
+++ meat-app-starter-master/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { NotificationService } from '../notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
@@ -31,11 +33,11 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
+    this.notificationService.notifier.do(message => {
       this.message = message;
       this.snackVisibility = "visible";
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = "hidden");
-    });
+    }).switchMap(message => Observable.timer(3000))
+    .subscribe(timer => this.snackVisibility = "hidden");
   }
 
 }
